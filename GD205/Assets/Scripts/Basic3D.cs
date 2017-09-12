2599f104dda58ec870e14b7525649acc71c3c1d8
GVWassa <libertysone@gmail.com>
2017-09-12 10:33:59 -0400

    this is the game project

---
+++ GD205/Assets/Scripts/Basic3D.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Basic3D : MonoBehaviour {
+
+	public GameObject mover;
+	public Vector3 mvtamount;
+
+	// Use this for initialization
+	void Start () {
+	mvtamount =new Vector3 (1,0,0);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	if(Input.GetKeyDown(KeyCode.Space)) { Debug.Log ("true");
+	mover.transform.position += mvtamount; } 
+
+	if(Input.GetKeyDown(KeyCode.W))  { Debug.Log ("true");
+	mover.transform.position -= mvtamount; } 
+
+
+	else { Debug.Log ("false");}
+
+
+	}
